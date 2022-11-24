a945516468f00e3060039841b44f238a4316093c
Lin <lj233@cornell.edu>
2022-11-23 21:50:44 -0500

    start implementing db.py and app.py for user

---
+++ app.py
@@ -0,0 +1,97 @@
+from re import T
+from db import Item, User, db
+from flask import Flask, request
+import json 
+import os
+
+
+app = Flask(__name__)
+db_filename = "cms.db"
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///%s" % db_filename
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+
+
+def success_response(data, code=200):
+    return json.dumps({"success": True, "data": data}, default=str), code
+
+def failure_response(message, code=404):
+    return json.dumps({"success": False, "error": message}, default=str), code
+
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+
+# your routes here
+
+@app.route("/api/users/", methods=["POST"])
+def create_user():
+    """
+    create a user
+    """
+    body = json.loads(request.data)
+    uname=body.get('username')
+    e=body.get('email')
+    if uname is None or e is None:
+        return failure_response("please provide a username and email",400)
+    new_user = User(
+        username= uname,
+        email=e,
+        credit=20,
+        rating=0,
+        profile_image_url=None,
+        )
+
+    db.session.add(new_user)
+    db.session.commit()
+    return success_response(new_user.serialize(), 201)
+
+@app.route("/api/users/<int:user_id>/")
+def get_user(user_id):
+    """
+    get a specific user
+    """
+    user = User.query.filter_by(id=user_id).first()
+    if user is None:
+        return failure_response("user not found")
+    return success_response(user.serialize())
+
+
+@app.route("/api/users/<int:user_id>/delete/", methods = ['DELETE'])
+def delete_user(user_id):
+    """
+    delete a user
+    """
+    user=User.query.filter_by(id=user_id).first()
+    if user is None:
+        return failure_response("user not found", 404)
+    db.session.delete(user)
+    db.session.commit()
+    return success_response(user.serialize(), )
+
+
+@app.route("/api/users/<int:user_id>/", methods = ['POST'])
+def update_user(user_id):
+    """
+    update an user with id user_id
+    """
+    user = User.query.filter_by(id= user_id).first()
+    if user is None:
+        return failure_response("user not found")
+    body = json.loads(request.data)
+    uname = body.get("username")
+    e = body.get("email")
+    image=body.get("profile_image_url")
+    if uname is not None:
+        user.username = uname
+    if e is not None:
+        user.email =e
+    if image is not None:
+        user.profile_image_url = image
+
+    return success_response(user.serialize(), 201)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
+
---
+++ db.py
@@ -0,0 +1,81 @@
+from flask_sqlalchemy import SQLAlchemy
+from enum import Enum
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable = False)
+    email = db.Column(db.String, nullable = False)
+    credit = db.Column(db.Integer, nullable = False)
+    rating = db.Column(db.Integer, nullable = False)
+    profile_image_url= db.Column(db.String)
+    lend_items = db.relationship("Item", cascade="delete")
+    borrow_items = db.relationship("Item", cascade="delete")
+
+    def serialize(self):
+        """
+        constructs users with all fields as a python dictionary
+        """
+        return {
+            "id": self.id,
+            "username": self.username,
+            "email": self.email,
+            "credit": self.credit,
+            "rating": self.rating,
+            "profile_image_url": self.profile_image_url,
+            "lend_items": [a.partial_serialize() for a in self.lend_items], 
+            "borrow_items":[a.partial_serialize() for a in self.borrow_items]
+        }
+    def partial_serialize(self):
+        """
+        constructs users without course field as a python dictionary
+        """
+        return {
+            "id": self.id,
+            "username": self.username,
+            "credit": self.credit,
+            "rating": self.rating,
+            "profile_image_url": self.profile_image_url
+        }
+
+class Item(db.Model):
+    __tablename__ = "items"
+    id = db.Column(db.Integer, primary_key=True)
+    itemname = db.Column(db.String, nullable = False)
+    lender_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    borrower_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    end_date = db.Column(db.DateTime(timezone=True), nullable = False)
+    return_date = db.Column(db.DateTime(timezone=True), nullable = False)
+    credit_value = db.Column(db.Integer, nullable = False)
+    post_type = db.Column(db.Boolean, nullable = False)
+    post_status = db.Column(db.Boolean, nullable = False)
+
+    def serialize(self):    
+        """
+        constructs assignment with all fields as a python dictionary
+        """
+        if self.post_type == True:
+            return {        
+                "id": self.id,        
+                "itemname": self.itemname,        
+                "due_date": self.due_date, 
+                "lender": User.query.filter_by(id = self.lender_id).first().partial_serialize(),
+                "end_date": self.end_date,
+                "credit_value": self.credit_value,
+                "post_type":"Lend",
+                "post_status": "Unfulfilled"}
+  
+        return {        
+        "id": self.id,        
+        "itemname": self.itemname,        
+        "due_date": self.due_date, 
+        "borrower": User.query.filter_by(id = self.borrower_id).first().partial_serialize(),
+        "return_date": self.return_date,
+        "credit_value": self.credit_value,
+        "post_type":"Borrow",
+        "post_status": "Unfulfilled"
+    }
+
+
