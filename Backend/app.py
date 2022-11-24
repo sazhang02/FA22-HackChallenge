957d256a68d8b6109a3c4bb7a90a0490a8c4f0ab
Samantha Zhang <sjz46@cornell.edu>
2022-11-23 23:51:23 -0500

    json dump necessary info

--- Backend/app.py
+++ Backend/app.py
@@ -12,12 +12,11 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///%s" % db_filename
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = True
 
-
 def success_response(data, code=200):
-    return json.dumps({"success": True, "data": data}, default=str), code
+    return json.dumps(data), code
 
 def failure_response(message, code=404):
-    return json.dumps({"success": False, "error": message}, default=str), code
+    return json.dumps({"error": message}), code
 
 db.init_app(app)
 with app.app_context():
